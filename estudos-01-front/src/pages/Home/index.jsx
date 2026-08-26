b0f35f80dd4c1c0dbf9d992518996a54f17b6104
Pechebiloski <devpechebiloski@gmail.com>
2026-08-26 15:35:37 -0300

    feat: add axios for API requests and implement user registration form
    - Added axios as a dependency for making HTTP requests.
    - Created an API service file to configure axios with the base URL.
    - Updated Home component to include a user registration form with input fields for name, age, and email.
    - Implemented functionality to fetch users from the API and display them in a list.
    - Added a trash icon for deleting users.
    - Updated styles for the form and user cards.
    - Removed unnecessary CSS and adjusted layout for better responsiveness.

--- estudos-01-front/src/pages/Home/index.jsx
+++ estudos-01-front/src/pages/Home/index.jsx
@@ -1,10 +1,49 @@
 import "./style.css";
+import Trash from '../../assets/trash.svg'
+//para importar imgs 
+//voce nao deve fazer como faz
+//no html padrão, precisa declarar 
+//a importaçao mo começo
+//e usar {} no src 
+import api from '../../services/api';
 
 function Home() {
+ let users = []
+ 
+ async function getUsers(){
+  users = await api.get('/users')
+ }
+
   return (
-    <div>
-      <h1>Olá react</h1>
+    <div className="container">
+      <form>
+        <h1>Cadastro de usuarios</h1>
+       <input placeholder="Nome" name="nome" type="text" />
+       <input placeholder="Idade" name="idade" type="number" />
+       <input placeholder="Email" name="email" type="email" />
+
+       <button type="button">Cadastrar</button>
+      </form>
+
+
+      {users.map( user => (
+        <div key={user.id} className="card">
+
+         <div>
+          <p>Nome:  <span>{user.name}</span></p>
+          <p>Idade: <span>{user.age}</span></p>
+          <p>Email: <span>{user.email}</span></p>
+         </div>
+         <button className="delete-btn">
+          <img src={Trash} alt="Trash for delete" />
+         </button>
+      </div>
+      ))}
+
+
     </div>
+ 
+  
   );
 }
 
